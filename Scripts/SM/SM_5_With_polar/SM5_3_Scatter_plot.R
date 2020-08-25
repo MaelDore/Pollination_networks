c3a3d09b3c6dc8c89e7d7c9f1c590ff9782b49d4
MAËL DORE <mael.dore3@mnhn.fr>
2020-08-25 16:39:17 +0200

    Final commit

--- Scripts/SM/SM_5_With_polar/SM5_3_Scatter_plot.R
+++ Scripts/SM/SM_5_With_polar/SM5_3_Scatter_plot.R
@@ -37,6 +37,7 @@ aggreg.webs <- readRDS(file = paste0("./Data/Filtered_Datasets/aggreg.webs_full_
 library(tidyverse)
 library(scales)
 library(ggthemes)
+library(cowplot)
 
 limits_df <- tibble(x = c(0, 100), y = c(-20, 30))
 polar_networks <- aggreg.webs[aggreg.webs$Latitude_dec > 70, ]
@@ -85,4 +86,51 @@ pdf(file = paste0("./SM_Final_figures/Figure_S5.1.pdf"), width = 12, height = 8)
 print(g1)
 
 dev.off()
-  
+
+### 3D plot with climate as 2 PCA-axes ####
+
+library(ade4) # Package utile pour l'ACP qui contient des jeux de données quantis
+
+aggreg.webs <- readRDS(file = paste0("./Data/Filtered_Datasets/aggreg.webs_full_rich_with_polar.RData"))
+
+polar_indices <- which(aggreg.webs$Latitude_dec > 70)
+
+# Extract climate variables
+
+climate_var <- aggreg.webs[, c("Mean_T_IPCC", "T_Seasonality_IPCC", "Tot_Rainfall_IPCC", "Rainfall_Seasonality_IPCC")]
+
+PCA_climate <- dudi.pca(df = climate_var, scannf = F, nf = 2) # Generate PCA and keep only 2 axes
+
+round(PCA_climate$eig/sum(PCA_climate$eig)*100, 2) # % Explained variance per axis
+cumsum(round(PCA_climate$eig/sum(PCA_climate$eig)*100, 2)) # % Cumulative explained variance per axis
+
+s.corcircle(PCA_climate$co, xax = 1, yax = 2)
+
+plot(PCA_climate$li, col = "black", pch = 16, main = "Climate ordination", xlab = paste0("PC1 (", round(PCA_climate$eig/sum(PCA_climate$eig)*100,2)[1] ," %)"), ylab = paste0("PC2 (", round(PCA_climate$eig/sum(PCA_climate$eig)*100,2)[2] ," %)"))
+points(PCA_climate$li[polar_indices, ], pch = 16, cex = 1.5, col = "red")
+# Ajouter les flèches des variables # Attention, la version correcte serait de récupérér les coordonnées transformées du biplot
+arrows(x0 = rep(0, nrow(PCA_climate$co)), y0 = rep(0, nrow(PCA_climate$co)), x1 = PCA_climate$co[,1]*3, y1 =  PCA_climate$co[,2]*3, length = 0.2, code = 2, lwd = 1)
+text(PCA_climate$co*3.3, labels = c("Tmean", "Tvar", "Rtot", "Rvar"))
+
+library(rgl)
+
+ThreeD_axis <- cbind(aggreg.webs$ln_HF, PCA_climate$li)
+
+col_pts <- rep("black", nrow(aggreg.webs)) ; col_pts[polar_indices] <- "red"
+labels_pts <- rep("", nrow(aggreg.webs)) ; labels_pts[polar_indices] <- All_coord[polar_indices]
+
+# Open 3D window with define orientation
+open3d(zoom = 1, 
+       userMatrix = matrix(data = c(0.639, 0.769, 0.004, 0, -0.165, 0.133, 0.978, 0, 0.752, -0.625, 0.211, 0, 0, 0, 0, 1) , nrow = 4, ncol = 4, byrow = T),
+       windowRect = c(0, 23, 1920, 1040),
+       viewport = c(0, 0, 1811, 1017))
+plot3d(x = ThreeD_axis, type = "n", xlab = "HII", ylab = "Climate PC1", zlab = "Climate PC2") # To generate the axis
+spheres3d(x = ThreeD_axis, radius = 0.05, col = "black") # To add points in the desired color in the 3D plot
+spheres3d(x = ThreeD_axis[polar_indices, ], radius = 0.1, col = "red")
+# text3d(x = ThreeD_axis[polar_indices, ], texts = All_coord[polar_indices], col = "red") # To add the labels in the desired color in the 3D plot
+
+# Get current 3D window parameters
+zoom <- par3d()$zoom ; userMatrix <- par3d()$userMatrix ; windowRect <- par3d()$windowRect ; viewport <- par3d()$viewport
+
+
+
